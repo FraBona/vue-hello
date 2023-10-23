2e37391bbb9686e0faec398f4783df82379e7c39
FraBona <francescobonafede08@gmail.com>
2023-10-23 15:24:55 +0200

    insert image at dom with vue

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ const { createApp } = Vue;
 createApp({
   data(){
     return{
-      message: 'Ciao sono Francesco'
+      message: 'Ciao sono Francesco',
+      src:  "./img 2/angela-caroll-chief-editor.jpg"
     }
   }
 }).mount('#app');
